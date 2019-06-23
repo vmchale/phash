fb216e863028648a63327e0c9637ede44437760c
Vanessa McHale <vanessa.mchale@iohk.io>
2019-06-23 12:17:16 -0500

    Make app better

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,9 @@
 module Main (main) where
 
+import           Data.Foldable              (foldrM)
+import           Data.List.NonEmpty         (NonEmpty (..), (<|))
+import qualified Data.Map                   as M
+import           Data.Word                  (Word64)
 import           PerceptualHash             (fileHash)
 import           System.Directory.Recursive
 import           System.FilePath            (takeExtension)
@@ -8,10 +12,19 @@ imgExtension :: String -> Bool
 imgExtension ".jpg"  = True
 imgExtension ".jpeg" = True
 imgExtension ".png"  = True
-imgExtension ".gif"  = True
-imgExtension _       = False -- idgaf about TIFF
+imgExtension _       = False -- gif doesn't work with CImg AFAICT
+
+insertHash :: FilePath -> M.Map Word64 (NonEmpty FilePath) -> IO (M.Map Word64 (NonEmpty FilePath))
+insertHash fp hashes = do
+    hash <- fileHash fp
+    case M.lookup hash hashes of
+        Just others -> pure $ M.insert hash (fp <| others) hashes
+        Nothing     -> pure $ M.insert hash (fp :| []) hashes
+
+mkMap :: [FilePath] -> IO (M.Map Word64 (NonEmpty FilePath))
+mkMap = foldrM insertHash mempty
 
 main :: IO ()
 main = do
     images <- filter (imgExtension . takeExtension) <$> getDirRecursive "."
-    print =<< traverse fileHash images
+    print =<< mkMap images
