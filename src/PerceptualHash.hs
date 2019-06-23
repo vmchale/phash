73aa44833524c5bb2e9fcca3020d8a49f3d8ff73
Vanessa McHale <vanessa.mchale@iohk.io>
2019-06-23 14:45:40 -0500

    Use correct types

--- src/PerceptualHash.hs
+++ src/PerceptualHash.hs
@@ -3,12 +3,12 @@ module PerceptualHash ( fileHash ) where
 import           Control.Applicative   (pure)
 import           Data.Word             (Word64)
 import           Foreign.C.String      (CString, withCString)
-import           Foreign.C.Types       (CInt (..))
+import           Foreign.C.Types       (CInt (..), CULLong)
 import           Foreign.Marshal.Alloc (alloca)
 import           Foreign.Ptr           (Ptr)
 import           Foreign.Storable      (peek)
 
-foreign import ccall ph_dct_imagehash :: CString -> Ptr Word64 -> IO CInt
+foreign import ccall ph_dct_imagehash :: CString -> Ptr CULLong -> IO CInt
 
 -- | @.gif@ format not supported.
 fileHash :: FilePath -> IO Word64
@@ -16,7 +16,7 @@ fileHash fp = withCString fp $ \cstr ->
     alloca $ \hashPtr -> do
         res <- ph_dct_imagehash cstr hashPtr
         check res
-        peek hashPtr
+        fromIntegral <$> peek hashPtr
 
     where check (-1) = error ("Hash of file " ++ fp ++ " failed.")
           check 0    = pure ()
