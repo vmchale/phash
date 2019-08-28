a0ccb3daef97a8d416f49605959258adeef7fc1b
Vanessa McHale <vanessa.mchale@iohk.io>
2019-08-28 18:12:50 -0500

    Build

--- app/Main.hs
+++ app/Main.hs
@@ -36,4 +36,4 @@ run (fps, debug) = do
     let displayF = if debug
         then displayDebug
         else displayAll . filterDup
-    putStrLn . displayF =<< pathMaps debug fps
+    putStrLn . displayF =<< pathMaps fps
