f829d7d893fe2afd8a531575b1ad22154559cb73
Vanessa McHale <vanessa.mchale@iohk.io>
2019-08-28 17:34:50 -0500

    Add debug + image formats

--- app/Parallel.hs
+++ app/Parallel.hs
@@ -2,6 +2,7 @@ module Parallel ( pathMaps ) where
 
 import           Control.Concurrent.STM      (atomically)
 import           Control.Concurrent.STM.TVar (TVar, modifyTVar', newTVarIO, readTVarIO)
+import           Control.Monad               (when)
 import           Data.List.NonEmpty          (NonEmpty (..), (<|))
 import qualified Data.Map                    as M
 import           Data.Word                   (Word64)
@@ -13,7 +14,14 @@ imgExtension :: String -> Bool
 imgExtension ".jpg"  = True
 imgExtension ".jpeg" = True
 imgExtension ".png"  = True
-imgExtension _       = False -- gif doesn't work with CImg AFAICT
+imgExtension ".gif"  = True
+imgExtension ".ppm"  = True
+imgExtension ".hdr"  = True
+imgExtension ".bmp"  = True
+imgExtension ".TGA"  = True
+imgExtension ".tif"  = True
+imgExtension ".tiff" = True
+imgExtension _       = False
 
 insertHash :: FilePath -> IO (M.Map Word64 (NonEmpty FilePath) -> M.Map Word64 (NonEmpty FilePath))
 insertHash fp = do
@@ -23,15 +31,17 @@ insertHash fp = do
             Just others -> M.insert hash (fp <| others) hashes
             Nothing     -> M.insert hash (fp :| []) hashes
 
-stepMap :: TVar (M.Map Word64 (NonEmpty FilePath)) -> FilePath -> IO ()
-stepMap var fp = do
+stepMap :: Bool -> TVar (M.Map Word64 (NonEmpty FilePath)) -> FilePath -> IO ()
+stepMap dbg var fp = do
+    when dbg $
+        putStrLn fp
     mod' <- insertHash fp
     atomically $ modifyTVar' var mod'
 
-pathMaps :: [FilePath] -> IO (M.Map Word64 (NonEmpty FilePath))
-pathMaps fps = do
+pathMaps :: Bool -> [FilePath] -> IO (M.Map Word64 (NonEmpty FilePath))
+pathMaps dbg fps = do
     total <- newTVarIO mempty
-    parTraverse (stepMap total) fileFilter (\_ -> pure True) fps
+    parTraverse (stepMap dbg total) fileFilter (\_ -> pure True) fps
     readTVarIO total
 
     where fileFilter = pure . imgExtension . takeExtension
