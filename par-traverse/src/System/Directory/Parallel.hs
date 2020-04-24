2a12c7c1d13668a17744305ab4c3274312614f88
Vanessa McHale <vamchale@gmail.com>
2020-04-24 14:07:57 -0500

    Add parTraverseAll

--- par-traverse/src/System/Directory/Parallel.hs
+++ par-traverse/src/System/Directory/Parallel.hs
@@ -1,4 +1,6 @@
-module System.Directory.Parallel ( parTraverse ) where
+module System.Directory.Parallel ( parTraverse
+                                 , parTraverseAll
+                                 ) where
 
 import           Control.Concurrent                  (getNumCapabilities)
 import           Control.Concurrent.ParallelIO.Local (Pool, parallel_, withPool)
@@ -17,21 +19,40 @@ partitionM f (x:xs) = do
         then pure (x:as, bs)
         else pure (as, x:bs)
 
-parTraverse :: (FilePath -> IO ()) -- ^ Action to execute on files
-            -> (FilePath -> IO Bool) -- ^ Filter on files
-            -> (FilePath -> IO Bool) -- ^ Filter on directories
-            -> [FilePath] -- ^ Starting directories
-            -> IO ()
-parTraverse act fileP dirP dirs = do
+-- | @since 0.2.1.0
+parTraverseAll :: (FilePath -> IO ()) -- ^ Action to execute on files
+               -> (FilePath -> IO Bool) -- ^ Filter on files
+               -> (FilePath -> IO Bool) -- ^ Filter on directories
+               -> [FilePath] -- ^ Starting files/directories
+               -> IO ()
+parTraverseAll act fileP dirP fps = do
+    (dirs, files) <- partitionM doesDirectoryExist fps
+    parTraverseFiles act fileP dirP dirs files
+
+
+parTraverseFiles :: (FilePath -> IO ()) -- ^ Action to execute on files
+                 -> (FilePath -> IO Bool) -- ^ Filter on files
+                 -> (FilePath -> IO Bool) -- ^ Filter on directories
+                 -> [FilePath] -- ^ Starting directories
+                 -> [FilePath] -- ^ Starting files (won't be filtered)
+                 -> IO ()
+parTraverseFiles act fileP dirP dirs files = do
     ncpu <- getNumCapabilities
     withPool ncpu $ \pool ->
-        parallel_ pool $ fmap (loopPool pool) dirs
+        parallel_ pool $ fmap act files ++ fmap (loopPool pool) dirs
 
     where loopPool :: Pool -> FilePath -> IO ()
           loopPool pool fp = do
                 all' <- fmap (fp </>) <$> listDirectory fp
                 -- unsafeInterleaveIO? lol
-                (dirs', files) <- partitionM doesDirectoryExist all'
+                (dirs', files') <- partitionM doesDirectoryExist all'
                 dirs'' <- filterM dirP dirs'
-                files' <- filterM fileP files
-                parallel_ pool (fmap act files' ++ fmap (loopPool pool) dirs'')
+                files'' <- filterM fileP files'
+                parallel_ pool (fmap act files'' ++ fmap (loopPool pool) dirs'')
+
+parTraverse :: (FilePath -> IO ()) -- ^ Action to execute on files
+            -> (FilePath -> IO Bool) -- ^ Filter on files
+            -> (FilePath -> IO Bool) -- ^ Filter on directories
+            -> [FilePath] -- ^ Starting directories
+            -> IO ()
+parTraverse act fileP dirP dirs = parTraverseFiles act fileP dirP dirs []
