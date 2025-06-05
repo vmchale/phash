d510a0001ade4ae20d903534237063f93150246c
Vanessa McHale <vamchale@gmail.com>
2025-06-04 22:54:59 -0400

    er

--- app/Main.hs
+++ app/Main.hs
@@ -9,23 +9,23 @@ import           Options.Applicative (execParser)
 import           Parallel
 import           Parser
 
-displayPaths :: NonEmpty FilePath -> String
-displayPaths = intercalate ", " . toList
+displayPaths :: NonEmpty (FilePath, (Int, Int)) -> String
+displayPaths = intercalate ", " . toList . fmap (\(fp, (x,y)) -> fp ++ "(" ++ show x ++ "x" ++ show y ++ ")")
 
-displayHash :: NonEmpty FilePath -> Word64 -> String
+displayHash :: NonEmpty (FilePath, (Int, Int)) -> Word64 -> String
 displayHash fps h = show h ++ " " ++ displayPaths fps
 
-filterDup :: M.Map Word64 (NonEmpty FilePath) -> M.Map Word64 (NonEmpty FilePath)
+filterDup :: M.Map Word64 (NonEmpty (FilePath, (Int, Int))) -> M.Map Word64 (NonEmpty (FilePath, (Int, Int)))
 filterDup = M.filter p
-    where p :: NonEmpty FilePath -> Bool
+    where p :: NonEmpty (FilePath, (Int, Int)) -> Bool
           p (_ :| (_:_)) = True
           p _            = False
 
-displayDebug :: M.Map Word64 (NonEmpty FilePath) -> String
+displayDebug :: M.Map Word64 (NonEmpty (FilePath, (Int, Int))) -> String
 displayDebug hashes = intercalate "\n" (mkLine <$> M.toList hashes)
     where mkLine (h, fps) = displayHash fps h
 
-displayAll :: M.Map a (NonEmpty FilePath) -> String
+displayAll :: M.Map a (NonEmpty (FilePath, (Int, Int))) -> String
 displayAll fps = intercalate "\n" (displayPaths <$> toList fps)
 
 main :: IO ()
